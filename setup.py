84866eb09bce87523b2bc6a534480eec000ba844
Oliver K. Ernst <oernst@ucsd.edu>
2019-06-20 16:43:20 -0700

    version no

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='SamplePairsGaussian',
-    version='1.0',
+    version='1.1',
     author='Oliver K. Ernst',
     packages=['samplePairsGaussian','examples'],
     install_requires=[
